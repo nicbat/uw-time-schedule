d9a81ea23faee97d0c23e5f8528ef6a1499c28bd
nicbat <nbatch@outlook.com>
2023-10-12 00:40:50 -0700

    fixed multi day classes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.get("https://www.washington.edu/students/timeschd/AUT2023/chem.html")
+r = requests.get("https://www.washington.edu/students/timeschd/AUT2023/cse.html")
 soup = BeautifulSoup(r.content, features="html.parser")
 
 # print(soup)
@@ -14,24 +14,30 @@ for table in tables:
     children = [child.string for child in table.descendants if child.name == None]
     children.pop(0)
     if 'pre' in [child.name for child in table.descendants]:
+        links = table.find_all("a")
+        if len(links) == 1:
+            continue
         obj = {}
-        # print(children)
-        try:
-            if children[1].split()[2] == "to":
-                continue
-            # TODO Hybrid lectures have multiple listings for times that they happen
-            obj["sln"] = int(children[0])
-            obj["letter"] = children[1].split()[0]
-            obj["credits"] = (children[1].split()[1])
-            obj["days"] = children[1].split()[2]
-            obj["times"] = children[1].split()[3]
-            obj["building"] = children[2]
-            obj["room"] = children[3].split()[0]
-        except:
-            print("error on", children)
-            # exit
+        if children[1].split()[2] == "to":
+            continue
+        obj["sln"] = int(links[0].contents[0])
+        links.pop(0)
+        obj["letter"] = children[1].split()[0]
+        obj["credits"] = (children[1].split()[1])
+        obj["locations"] = [{"days": children[1].split()[2], "time": children[1].split()[3], "building": links[0].contents[0], "room": children[3].split()[0]}]
+        links.pop(0)
+        additional = 0
+        while len(links) > 0:
+            times = children[3 + (2*additional)].split("\n")[1].split()
+            extra = {}
+            extra["days"] = times[0]
+            extra["time"] = times[1]
+            extra["building"] = links[0].contents[0]
+            extra["room"] = children[5 + (2*additional)].split()[0]
+            obj["locations"].append(extra)
+            links.pop(0)
         current[-1]["classes"].append(obj)
     else:
         current.append({"name": " ".join(children[0].split()), "classes":[]})
 for c in current:
-    print(c["name"], [d["sln"] for d in c["classes"]])
+    print(c["name"], [d for d in c["classes"]])
